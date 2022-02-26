f548dcd9527cff0881391817ad3105896c3f1772
206 <206@academicos.cesde.edu.co>
2022-02-25 20:46:24 -0500

    NTV001 Se realizaron las validaciones de la variable del nivel de agua

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 print("Viva Putin")
 
 nivelAgua=int(input("Digita la cantidad de agua de la repreasa: "))
-print(f"El nivel de agua es {nivelAgua}")
-
-print("OE socio ")
+if(nivelAgua<200):
+    print(f"No tengo agua")
+elif (nivelAgua>= 200 and nivelAgua<=400):
+    print(f"Energia corriente")
+else:
+    print(f"ojo con caucasia >.<")
