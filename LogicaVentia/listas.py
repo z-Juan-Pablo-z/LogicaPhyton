4d9719224c8634c14526dbf5265a63ef2f3618bd
206 <206@academicos.cesde.edu.co>
2022-03-04 20:55:58 -0500

    NTV002 Se crea ejemplo de lista

---
+++ LogicaVentia/listas.py
@@ -0,0 +1,10 @@
+from unicodedata import numeric
+
+
+numeros=[1,2,3,4,5]
+print(numeros[2])
+
+#for i in range(1,20,4):
+#               inicia/termina/incrementa
+for i in range(20):
+    print(i)
